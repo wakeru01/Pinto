e6070467045024c0743713db93f001a7d5cb9363
wakeru01 <42973275+wakeru01@users.noreply.github.com>
2020-04-23 22:07:23 +0700

    edit

--- Co-Working-Space-master/co_working/management/models.py
+++ Co-Working-Space-master/co_working/management/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=100)
-    phone_number = models.CharField(max_length = 10)
+    phone_number = models.CharField(max_length=10, default='0')
 
 class Member(models.Model):
     first_name = models.CharField(max_length = 30)
