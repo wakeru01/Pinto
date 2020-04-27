99877123b42eccea32abcb85b27944d93e2a351d
Ratchapat Permpool <permpool3@gmail.com>
2020-04-28 01:59:28 +0700

    Remove unneccesary file

--- Co-Working-Space-master/co_working/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\inwni\documents\github\co-working-space\co_working\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
